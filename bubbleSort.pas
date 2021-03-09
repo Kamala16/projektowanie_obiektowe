1af058d2d2e0ed60b8aeb37cfa837659c61c3c44
Aleksandra Chrzanowska <aleksandra.chrzanowska@student.uj.edu.pl>
2021-03-09 12:20:03 +0100

    Dodaj sortowanie babelkowe

---
+++ bubbleSort.pas
@@ -0,0 +1,42 @@
+PROGRAM sortowanieBabelkowe;
+
+TYPE
+  list = array[1..100] of Integer;
+
+VAR
+  lista: list;
+  i, j, temp: Integer;
+  n: Integer = 100;
+  len: Integer = 20;
+
+PROCEDURE sort;
+BEGIN
+  For i:=1 To len - 1 Do
+  Begin
+    For j:=1 To len - i Do
+    Begin
+      if lista[j] > lista[j + 1] then begin
+         temp := lista[j];
+         lista[j] := lista[j + 1];
+	     lista[j + 1] := temp;
+      end;
+    End;
+  End;
+END;
+
+BEGIN
+
+  For i:=1 To len Do
+  Begin
+    lista[i] := Random(n)+1;
+    write(lista[i], ' ');
+  End;
+  writeln();
+  writeln('POSORTOWANE');
+  sort;
+  For i:=1 To len Do
+  Begin
+    write(lista[i], ' ');
+  End;
+  writeln();
+END.
