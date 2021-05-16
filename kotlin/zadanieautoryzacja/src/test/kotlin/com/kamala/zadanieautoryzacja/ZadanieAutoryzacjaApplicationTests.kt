9b5513235885c7f0a7e81f3f2db123ac8d883627
Aleksandra Chrzanowska <aleksandra.chrzanowska@student.uj.edu.pl>
2021-05-16 22:38:53 +0200

    popraw zapaszki

--- kotlin/zadanieautoryzacja/src/test/kotlin/com/kamala/zadanieautoryzacja/ZadanieAutoryzacjaApplicationTests.kt
+++ kotlin/zadanieautoryzacja/src/test/kotlin/com/kamala/zadanieautoryzacja/ZadanieAutoryzacjaApplicationTests.kt
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class ZadanieAutoryzacjaApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() { //print("OK")
+    }
 }
