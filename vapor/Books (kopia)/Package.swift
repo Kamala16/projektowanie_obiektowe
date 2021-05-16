aef478a53a3702bcb3b4a0fd89c5ab06c920077c
Aleksandra Chrzanowska <aleksandra.chrzanowska@student.uj.edu.pl>
2021-05-16 23:21:52 +0200

    popraw zapaszki

--- vapor/Books (kopia)/Package.swift
+++ vapor/Books (kopia)/Package.swift
@@ -1,10 +1,6 @@
 // swift-tools-version:5.2
 import PackageDescription
 
-var vapor = "https://github.com/vapor/vapor.git"
-var fluent = "https://github.com/vapor/fluent.git"
-var sqlite = "https://github.com/vapor/fluent-sqlite-driver.git"
-var leaf = "https://github.com/vapor/leaf.git"
 
 let package = Package(
     name: "Books",
@@ -13,6 +9,11 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
+        let vapor = "https://github.com/vapor/vapor.git"
+        let fluent = "https://github.com/vapor/fluent.git"
+        let sqlite = "https://github.com/vapor/fluent-sqlite-driver.git"
+        let leaf = "https://github.com/vapor/leaf.git"
+
         .package(url: vapor, from: "4.0.0"),
         .package(url: fluent, from: "4.0.0"),
         .package(url: sqlite, from: "4.0.0"),
